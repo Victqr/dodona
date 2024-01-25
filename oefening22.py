33b62cfd77268c5daee8654b61fe3c14b896e9ef
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2024-01-25 10:32:09 +0200

    Create oefening22.py

---
+++ oefening22.py
@@ -0,0 +1,9 @@
+getal1 = int(input("Geef het eerste gehele getal: "))
+getal2 = int(input("Geef het tweede gehele getal: "))
+
+if getal1 < getal2:
+    kleinste = getal1
+else:
+    kleinste = getal2
+
+print(f"{kleinste}")
