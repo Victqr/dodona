6a110f36c410894a43e056f4ca008ffaa52c327f
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2024-05-08 09:23:22 +0200

    Create 64.py

---
+++ 64.py
@@ -0,0 +1,11 @@
+getal1 = int(input("Voer het eerste getal in: "))
+getal2 = int(input("Voer het tweede getal in: "))
+getal3 = int(input("Voer het derde getal in: "))
+
+getallen = [getal1, getal2, getal3]
+
+getallen.sort()
+
+print(getallen[0])
+print(getallen[1])
+print(getallen[2])
